b52bb877143c5a5b0c5d0fe766c91b458f904419
natyyp <natypiresoliveira@gmail.com>
2023-05-08 17:12:44 -0300

    fodase

---
+++ ombegeto.js
@@ -0,0 +1,135 @@
+// const usario = {
+//     nome: "vitimnho",
+//      idade: "16",
+//      emdereso: "rua:sakemlmacla ,  numero:69 , bairro:kulsham",
+//      semha: "vadias"
+// }
+
+// usario.nome = "kashlam"
+// console.log(usario.nome);
+// console.log(usario);
+
+
+// const filme = {
+// 	nome: "John Wick 4: Baba Yaga", 
+// 	ano: 2023, 
+// 	elenco: [
+// 		"Keanu Reeves",
+//         "John Wick",
+//         'Scott Adkins',
+//         "Killa",
+
+//         "Donnie Yen",
+//         "Caine",
+
+//         "Bill Skarsgård",
+//         "Marquis Vincent de Gramont",
+
+//         "Lance Reddick",
+//         "Charon",
+
+//         "Rina Sawayama",
+//         "Shimazu Akira",
+
+//         "Ian McShane",
+//         "Winston",
+
+//         "Natalia Tena",
+//         'Katia',
+
+//         "Hiroyuki Sanada",
+//         "Shimazu Koji",
+
+//         "Marko Zaror",
+//         "Chidi",
+
+//         'Shamier Anderson',
+//         "Tracker",
+
+//         "Bridget Moynahan",
+//         "Helen"
+// 		], 
+// 	transmissoesHoje: [
+// 		{canal: "Telecine", horario: "21h"}, 
+// 		{canal: "Canal Brasil", horario: "19h"}, 
+// 		{canal: "Globo", horario: "14h"}
+// 		]
+// }
+
+// console.log(filme.elenco[0])
+// console.log(filme.elenco[filme.elenco.length - 1])
+// console.log(filme.transmissoesHoje[2])
+
+
+// const pesoa = {
+//     nome:"vito",
+//     idades:"69",
+//     generomsc:"trap"
+// }
+// console.log(`o nome da pessoa é ${pesoa.nome}, ele tem ${pesoa.idades} anos e gosta muito de ${pesoa.generomsc}`);
+
+
+// const endereso = {
+//     rua:"Vadia",
+//     numero:"69",
+//     bairro:"fodase",
+//     cidade:"não perguntei"
+// }
+// console.log(endereso)
+
+
+// const produto = [
+// {id:1,nome:"Teclado mecanico da razer",preco:200},
+// {id:2,nome:"monitor dell, 21 polegadas",preco:3000},
+// {id:3,nome:"mouse longitech g403",preco:400},
+// ]
+// produto[0].quantidade = 20
+// produto[1].quantidade = 110
+// produto[2].quantidade = 50
+// console.log(produto);
+
+
+// const id = prompt("digite o id do produto")
+// const nomeproduto = prompt("digite o nome do produto")
+// const preco = Number(prompt("digite o preço do produto"))
+
+// let produtos = [];
+
+// function adicionarproduto(id, nomeproduto, preco) {
+//     const produto = {
+//         idproduto: id,
+//         nome: nomeproduto,
+//         precoproduto: preco
+//     }
+
+//     produtos.push(produto)
+// }
+
+// adicionarproduto(id,nomeproduto,preco)
+
+// console.log(produtos);
+
+
+    //     const pesoa = {
+    //         nome:"jose",
+    //         idade:69,
+    //         genero:"rock"
+    //     }
+
+    //     function adicionapesoa(pesoa){
+    //         const nvpesoa = {
+    //          ...pesoa,
+    //         comidas:["arroz,feijão e batata"],
+    //         melhoamigo:{  
+    //         nome: "maria",            
+    //         idade: 22,
+    //     },
+
+    //     };
+       
+    //     return nvpesoa;
+       
+    // };
+        
+    // console.log(adicionapesoa(pesoa));
+    
